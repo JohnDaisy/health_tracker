b70098938487172496c5e0e4a13ed2e4512f20e3
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-06-05 09:29:43 -0400

    Add method to check for today's total caloric intake

--- health_tracker/app/models/intake_amount.rb
+++ health_tracker/app/models/intake_amount.rb
@@ -1,4 +1,13 @@
 class IntakeAmount < ActiveRecord::Base
-  
+
+  def total_intake_today
+    today = self.all.select{|s| s.date.day == DateTime.now.day}
+    today.reduce(0){|sum, t| sum + t.intake_amount}
+  end
+
+
+  # def net_calories
+  #   (self.all.reduce(0){|sum, t| sum + i.intake_amount}) - calories_burned
+  # end
 
 end
