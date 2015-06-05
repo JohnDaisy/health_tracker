93aaad2760497c2f11b9f467066fb234657c5bff
John M Buscemi <jmbuscemi@gmail.com>
2015-06-05 09:41:07 -0400

    Change step number total to be daily total

--- health_tracker/app/models/step.rb
+++ health_tracker/app/models/step.rb
@@ -1,11 +1,11 @@
 class Step < ActiveRecord::Base
 
-  def self.total
-    self.all.reduce(0){|sum, t| sum + t.step_number}
+  def self.daily_total
+    today = self.all.select{|s| s.date.day == DateTime.now.day}
+    today.reduce(0){|sum, t| sum + t.step_number}
   end
 
   def self.number_of_miles
-    today = self.all.select{|s| s.date.day == DateTime.now.day}
-    (today.reduce(0){|sum, s| sum + s.step_number} / 2112).round(2)
+    (self.daily_total / 2112).round(2)
   end
 end
