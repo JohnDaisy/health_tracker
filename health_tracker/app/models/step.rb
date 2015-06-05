0d288814c7cb63f6a90a0f40059e86a1ced9c59f
John M Buscemi <jmbuscemi@gmail.com>
2015-06-04 23:31:12 -0400

    Add number of miles walked method and test

--- health_tracker/app/models/step.rb
+++ health_tracker/app/models/step.rb
@@ -3,4 +3,9 @@ class Step < ActiveRecord::Base
   def self.total
     self.all.reduce(0){|sum, t| sum + t.step_number}
   end
+
+  def self.number_of_miles
+    today = self.all.select{|s| s.date.day == DateTime.now.day}
+    (today.reduce(0){|sum, s| sum + s.step_number} / 2112).round(2)
+  end
 end
--- health_tracker/test/models/step_test.rb
+++ health_tracker/test/models/step_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class StepTest < ActiveSupport::TestCase
   test "get total" do
-    assert_equal 300, Step.total
+    assert_equal 4224, Step.total
+  end
+
+  test "miles stepped" do
+    assert_equal 2, Step.number_of_miles
   end
 end
