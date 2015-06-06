b4b38f7d4f23e27baa14684a991799617301edcf
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-06-06 14:32:07 -0400

    Add methods and tests for net calories per day

--- health_tracker/test/models/intake_amount_test.rb
+++ health_tracker/test/models/intake_amount_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class IntakeAmountTest < ActiveSupport::TestCase
   test "intake_amount total" do
-    assert_equal 1400, IntakeAmount.daily_intake
+    assert_equal 1200, IntakeAmount.daily_intake.to_f
   end
+
+  test "net calories today" do
+    assert_equal 980, IntakeAmount.net_calories
+  end
+
 end
